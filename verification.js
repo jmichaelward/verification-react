29cfc60084037462514e2072ecf99e429ed36dd9
Jeremy Ward <jeremy@jmichaelward.com>
2021-08-06 11:47:43 -0500

    Create verification.js.

---
+++ verification.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    "id": 1,
+    "name": "App Module",
+    "module": "kafka",
+    "createdAt": "Fri Aug 06 16:45:55 2021 UTC",
+    "modifiedAt": "Fri Aug 06 16:45:55 2021 UTC"
+  },
+  {
+    "id": 2,
+    "name": "Product Library",
+    "module": "kubernetes",
+    "createdAt": "Fri Aug 06 16:45:55 2021 UTC",
+    "modifiedAt": "Fri Aug 06 16:45:55 2021 UTC"
+  }
+];
