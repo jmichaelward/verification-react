0373638c7442c53ba1ef5515575083ca973cfc0c
Jeremy Ward <jeremy@jmichaelward.com>
2021-08-06 11:49:59 -0500

    Move verifications into src and rename.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import verifications from './verifications';
 
 function App() {
+  console.log(verifications);
   return (
     <div className="App">
       <header className="App-header">
