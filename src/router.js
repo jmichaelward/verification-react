a02e816be3161bc8bfc54ccd2dc11b500c918dd0
Jeremy Ward <jeremy@jmichaelward.com>
2021-08-06 13:15:28 -0500

    Rename unknown route to not found route.

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import {route as HomeRoute} from './routes/home';
 import {route as VerificationRoute} from './routes/verification';
-import {route as UnknownRoute} from './routes/404';
+import {route as NotFoundRoute} from './routes/404';
 import verifications from './data/verifications';
 
 export const AppRouter = () => {
@@ -24,7 +24,7 @@ export const AppRouter = () => {
           }
 
           <Route path="/*">
-            <UnknownRoute/>
+            <NotFoundRoute/>
           </Route>
         </Switch>
       </Router>
