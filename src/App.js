753bf9a6994a0687be68226651d7563b27cec327
Jeremy Ward <jeremy@jmichaelward.com>
2021-08-06 12:19:12 -0500

    Add routing based on data.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import verifications from './verifications';
 
+const Verification = (props) => <h2>{props.name}</h2>;
+
 function App() {
-  console.log(verifications);
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <Router>
+            <div>
+              <nav>
+                <ul>
+                  {
+                    verifications.map(verification => {
+                      const {id, name} = verification,
+                          href = `/verifications/${id}`;
+                      return <li><Link to={href}>{name}</Link></li>;
+                    })
+                  }
+                </ul>
+              </nav>
+            </div>
+
+            <Switch>
+              {
+                verifications.map(verification => {
+                  const href = `/verifications/${verification.id}`;
+                  return <Route path={href}><Verification name={verification.name}/></Route>;
+                })
+              }
+            </Switch>
+          </Router>
+        </header>
+      </div>
   );
 }
 
